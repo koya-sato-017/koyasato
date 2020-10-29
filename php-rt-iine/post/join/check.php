cd3a687ccc932f3904b632c89b4657121aae9254
koyasato <koya.sato.017@gmail.com>
2020-10-29 17:07:52 +0900

    remove: echoを削除

--- php-rt-iine/post/join/check.php
+++ php-rt-iine/post/join/check.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['join'])) {
 if (!empty($_POST)) {
     // 登録処理をする
     $statement = $db->prepare('INSERT INTO members SET name=?, email=?, password=?, picture=?, created=NOW()');
-    echo $ret = $statement->execute(array(
+    $ret = $statement->execute(array(
         $_SESSION['join']['name'],
         $_SESSION['join']['email'],
         sha1($_SESSION['join']['password']),
