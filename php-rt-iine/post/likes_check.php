78d4d14de4c218bb620789035be73b56de3fa4cc
koyasato <koya.sato.017@gmail.com>
2020-11-11 05:58:38 +0900

    remove: 不要な記述を削除

--- php-rt-iine/post/likes_check.php
+++ php-rt-iine/post/likes_check.php
@@ -1,11 +1,9 @@
 <?php
 // いいねが登録済みかチェックするため情報を取得する
 session_start();
-require('dbconnect.php');
 
 function likeCheck($db) 
 {    
-    global $db;
     if (isset($_SESSION['member_id'])) {
         $likes = $db->prepare('SELECT COUNT(like_post_id) AS li_cnt FROM likes WHERE like_post_id=? AND like_member_id=? GROUP BY like_post_id');
         $likes->execute(array(
